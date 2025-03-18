d973fc61c6042211a2bbe630d208abbf4d5fb9a6
TaaviAllingu <147393230+TaaviAllingu@users.noreply.github.com>
2025-03-18 13:53:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
 var data = [];
- 
- async function fetchWeatherData() {
-     const response = await fetch("https://api.met.no/weatherapi/locationforecast/2.0//forecast?latitude=59&longitude=26");
-     const weatherData = await response.json();
-     data = weatherData.hourly;
- 
- }
- 
- async function populateTable() {
-     await fetchWeatherData();
-     var table = document.getElementById('myTable');
-     
-     data.time.forEach(function(time, index) {
-         var row = table.insertRow();
-         var cell1 = row.insertCell(0);
-         var cell2 = row.insertCell(1);
-         cell1.innerHTML = time;
-         cell2.innerHTML = data.temperature_2m[index];
-     });
- }
- 
- populateTable();      
+
+async function fetchWeatherData() {
+    const response = await fetch("https://api.met.no/weatherapi/locationforecast/2.0/forecast?latitude=59&longitude=26");
+    const weatherData = await response.json();
+    data = weatherData.properties.timeseries; 
+}
+
+async function populateTable() {
+    await fetchWeatherData();
+    var table = document.getElementById('myTable');
+    
+    data.forEach(function(entry) {
+        var row = table.insertRow();
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        
+        var time = entry.time; 
+        var temperature = entry.data.instant.details.temperature; 
+        
+        cell1.innerHTML = time;
+        cell2.innerHTML = temperature;
+    });
+}
+
+populateTable();
